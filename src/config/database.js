d7559755076e9fba44721ec0e2fae8943d6a2ca6
alvinxrww <alvin.xavier@ui.ac.id>
2025-01-27 17:00:09 +0700

    fix: adjust db config to cloud sql

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,13 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-});
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Required for self-signed certs
+      }
+    },
+    logging: true // Set true for debugging
+  });
 
 module.exports = sequelize;
